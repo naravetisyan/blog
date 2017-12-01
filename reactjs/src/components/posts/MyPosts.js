b1a0eaee6d2d2de3df0816dda9b40cd1e14d48a3
narekavetisyan <nnnavetisyan@gmail.com>
2017-12-01 16:08:48 +0400

    MyPosts.js

--- reactjs/src/components/posts/MyPosts.js
+++ reactjs/src/components/posts/MyPosts.js
@@ -17,23 +17,23 @@ export default class MyPosts extends Component {
         this.editPost = this.editPost.bind(this);
         this.deletePost = this.deletePost.bind(this);
 
-	}
-	addPost(post) {
-		let posts = this.state.posts;
-	    posts.push(post);
-	    this.setState({posts});
-	}
+    }
+    addPost(post) {
+        let posts = this.state.posts;
+        posts.push(post);
+        this.setState({posts});
+    }
     editPost(post) {
-    	let posts = this.state.posts;
-    	posts.map((val,index) => {
-    		if(val.id == post.id){
-    			val = post;
-    		}
-    	})
-		this.setState({posts});
+        let posts = this.state.posts;
+        posts.map((val,index) => {
+            if(val.id == post.id){
+                val = post;
+            }
+        })
+        this.setState({posts});
     }
     deletePost(post) {
-    	let posts = this.state.posts;
+        let posts = this.state.posts;
         posts.map((value,index) => {
             if(value.id == post){
                 posts.splice(index,1);
@@ -41,59 +41,59 @@ export default class MyPosts extends Component {
         })
         this.setState({posts});
     }
-	componentWillMount() {
-		let url = '/api/me/posts';
-		axios.get(url).then((response) => {
+    componentWillMount() {
+        let url = '/api/me/posts';
+        axios.get(url).then((response) => {
             this.setState({
-				posts: response.data.my_posts
-			})
-		});
-	}
-  	render() {
-  		let posts = this.state.posts;
+                posts: response.data.my_posts
+            })
+        });
+    }
+    render() {
+        let posts = this.state.posts;
         return (
-			<div className="my-posts-all">
-				<h3>My Posts</h3>
-				<div className="list-group">
-					<AddPost user_id={this.props.user_id} addPost={this.addPost}/>
-					{	
-						posts.map((value, index) => {
-							let src = "../images/"+value.image;
-							return (
-								<div key={index}>
-									<div id="mine">
-										<div className="row">
-											<div className="span2" >
-												<a href="#" className="thumbnail bg-img">
-												<img width="260" height="180" src={src}/>
-												</a>
-											</div>
-											<div className="span6"> 
-												<h3>{value.title}</h3>     
-												<p className="p-1">{value.text}</p>
-												<p id="p-2">
-													#Category : {value.category.title}   
-												</p>
-											</div>
-											<EditPost  
-												id={value.id}
-												current_name_value={value.title} 
-												current_cat_value={value.category_id} 
-												current_text_value={value.text} 
-												editPost={this.editPost} 
-											/>
-										 	<DeletePost  
-											 	id={value.id} 
-											 	deletePost={this.deletePost} 
-										 	/>
-										</div>
-									</div>
-								</div>
-							)	
-						})
-					}
-				</div>
-			</div>
-    	)
+            <div className="my-posts-all">
+                <h3>My Posts</h3>
+                <div className="list-group">
+                    <AddPost user_id={this.props.user_id} addPost={this.addPost}/>
+                    {   
+                        posts.map((value, index) => {
+                            let src = "../images/"+value.image;
+                            return (
+                                <div key={index}>
+                                    <div id="mine">
+                                        <div className="row">
+                                            <div className="span2" >
+                                                <a href="#" className="thumbnail bg-img">
+                                                <img width="260" height="180" src={src}/>
+                                                </a>
+                                            </div>
+                                            <div className="span6"> 
+                                                <h3>{value.title}</h3>     
+                                                <p className="p-1">{value.text}</p>
+                                                <p id="p-2">
+                                                    #Category : {value.category.title}   
+                                                </p>
+                                            </div>
+                                            <EditPost  
+                                                id={value.id}
+                                                current_name_value={value.title} 
+                                                current_cat_value={value.category_id} 
+                                                current_text_value={value.text} 
+                                                editPost={this.editPost} 
+                                            />
+                                            <DeletePost  
+                                                id={value.id} 
+                                                deletePost={this.deletePost} 
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                            )   
+                        })
+                    }
+                </div>
+            </div>
+        )
     }
 }
